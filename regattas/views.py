2a8d2710f57a35aacdc209ba95c92a26ae1c5ff1
SepitleFTW <sepitle@verityafrica.co.za>
2026-02-12 12:36:19 +0200

    added ViewSets for CRUD/API

--- regattas/views.py
+++ regattas/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Regatta, Race, Result, Club
+from rest_framework import viewsets
+from .serializers import RegattaSerializer, RaceSerializer, ResultSerializer, ClubSerializer
 
 def index(request):
     return HttpResponse("Hello and welcome to beginning of regatta results!")
+
+# handling api logic for CRUD with ViewSets
+class RegattaViewSet(viewsets.ModelViewSet):
+    queryset = Regatta.objects.all()
+    serializer_class = RegattaSerializer
+
+class ResultViewSet(viewsets.ModelViewSet):
+    queryset = Result.objects.all()
+    serializer_class = ResultSerializer
+
+class RaceViewSet(viewsets.ModelViewSet):
+    queryset = Race.objects.all()
+    serializer_class = RaceSerializer
+
+class ClubViewSet(viewsets.ModelViewSet):
+    queryset = Club.objects.all()
+    serializer_class = ClubSerializer
