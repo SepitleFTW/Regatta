c25d17bcbde77b5091ed0e2b732f1ce6a74cc8fe
SepitleFTW <sepitle@verityafrica.co.za>
2026-02-12 12:57:47 +0200

    URLS work

--- regattas/urls.py
+++ regattas/urls.py
@@ -1,7 +1,20 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from  .views import RegattaViewSet, ResultViewSet, RaceViewSet, ClubViewSet
 from . import views
+
+"""
+registering  ViewSets so they get URLS
+"""
+
+router = DefaultRouter()
+router.register(r'regattas', RegattaViewSet)
+router.register(r'results', ResultViewSet)
+router.register(r'races', RaceViewSet)
+router.register('clubs', ClubViewSet)
+
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('api/', include(router.urls)),
 ]
